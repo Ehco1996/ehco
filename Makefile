db63e90c54faf5abc534955c5b553580456ce827
ehco1996 <zh19960202@gmail.com>
2021-08-28 20:55:23 +0800

    cmd: add more version info

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 NAME=ehco
 BINDIR=.dist
-VERSION=$(shell git describe --tags || echo "unknown version")
-BUILDTIME=$(shell date -u)
-GOBUILD=CGO_ENABLED=0 go build -trimpath -ldflags='-s -w -X="github.com/Ehco1996/ehco/internal/constant/constant.Version=$(VERSION)"'
+
+PACKAGE=github.com/Ehco1996/ehco/internal/constant
+BUILDTIME=$(shell date +"%m-%d-%y-%T")
+BRANCH=$(shell git rev-parse --abbrev-ref HEAD | tr -d '\040\011\012\015\n')
+REVISION=$(shell git rev-parse --short HEAD)
+
+# -w -s 参数的解释：You will get the smallest binaries if you compile with -ldflags '-w -s'. The -w turns off DWARF debugging information
+# for more information, please refer to https://stackoverflow.com/questions/22267189/what-does-the-w-flag-mean-when-passed-in-via-the-ldflags-option-to-the-go-comman
+GOBUILD=CGO_ENABLED=0 go build -trimpath -ldflags="-w -s -X ${PACKAGE}.GitBranch=${BRANCH} -X ${PACKAGE}.GitRevision=${REVISION} -X ${PACKAGE}.BuildTime=${BUILDTIME}"
 
 .PHONY: fmt test build tidy ensure release
 
--- internal/constant/constant.go
+++ internal/constant/constant.go
@@ -4,11 +4,34 @@ import "time"
 
 var (
 	DefaultDeadline = 30 * time.Second
+
+	Version     = "1.0.7"
+	GitBranch   string
+	GitRevision string
+	BuildTime   string
+
+	IndexHTMLTMPL = `<!doctype html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h2>ehco is a network relay tool and a typo :)</h2>
+		<hr>
+		<h3>Version: ` + Version + `</h3>
+		<h3>GitBranch: ` + GitBranch + `</h3>
+		<h3>GitRevision: ` + GitRevision + `</h3>
+		<h3>BuildTime: ` + BuildTime + `</h3>
+		<hr>
+		<p><a href="https://github.com/Ehco1996/ehco">More information here</a></p>
+		<p><a href="/metrics/">Metrics</a></p>
+		<p><a href="/debug/pprof/">Debug</a></p>
+	</body>
+	</html>
+	`
 )
 
 const (
-	Version = "1.0.7"
-
 	MaxMWSSStreamCnt = 100
 	DialTimeOut      = 3 * time.Second
 
@@ -25,19 +48,4 @@ const (
 	BUFFER_POOL_SIZE = 128      // 128 * 4kb
 	BUFFER_SIZE      = 4 * 1024 // 4kb
 
-	IndexHTMLTMPL = `<!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8">
-</head>
-<body>
-	<h2>Ehco(Version ` + Version + `)</h2>
-	<h3>ehco is a network relay tool and a typo :)</h3>
-	<p><a href="https://github.com/Ehco1996/ehco">More information here</a></p>
-
-	<p><a href="/metrics/">Metrics</a></p>
-	<p><a href="/debug/pprof/">Debug</a></p>
-</body>
-</html>
-`
 )
