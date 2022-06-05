eb9229dab24a95ef35c01d1b04f2e79fbd841c6e
ehco1996 <zh19960202@gmail.com>
2022-06-05 09:26:09 +0800

    xray: fix reutrn when user not found

--- pkg/xray/user.go
+++ pkg/xray/user.go
@@ -169,7 +169,7 @@ func (up *UserPool) syncTrafficToServer(ctx context.Context, endpoint string) er
 		user, found := up.GetUser(userID)
 		if !found {
 			logger.Logger.Warnf("user in xray not found in user pool this user maybe out of traffic, user id: %d", userID)
-			return nil
+			continue
 		}
 		switch trafficType {
 		case "uplink":
