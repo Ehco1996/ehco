d68d962700f90112fb081d1ef41abe82cf340472
Ehco1996 <zh19960202@gmail.com>
2024-02-18 13:51:56 +0800

    *: refactor conn test

--- internal/conn/conn.go
+++ internal/conn/conn.go
@@ -1,7 +1,10 @@
 package conn
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"net"
 	"time"
 
@@ -95,6 +98,44 @@ func (c innerConn) CloseRead() error {
 		return tcpConn.CloseRead()
 	}
 	return nil
+}
+
+func shortHashSHA256(input string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(input))
+	hash := hasher.Sum(nil)
+	return hex.EncodeToString(hash)[:7]
+}
+
+func connectionName(conn net.Conn) string {
+	return fmt.Sprintf("l:<%s> r:<%s>", conn.LocalAddr(), conn.RemoteAddr())
+}
+
+func copyConn(conn1, conn2 *innerConn) error {
+	errCH := make(chan error, 1)
+	// copy conn1 to conn2,read from conn1 and write to conn2
+	go func() {
+		_, err := io.Copy(conn2, conn1)
+		_ = conn2.CloseWrite() // all data is written to conn2 now, so close the write side of conn2 to send eof
+		errCH <- err
+	}()
+
+	// reverse copy conn2 to conn1,read from conn2 and write to conn1
+	_, err := io.Copy(conn1, conn2)
+	_ = conn1.CloseWrite()
+
+	err2 := <-errCH
+	_ = conn1.CloseRead()
+	_ = conn2.CloseRead()
+
+	// handle errors, need to combine errors from both directions
+	if err != nil && err2 != nil {
+		err = fmt.Errorf("transport errors in both directions: %v, %v", err, err2)
+	}
+	if err != nil {
+		return err
+	}
+	return err2
 }
 
 type RelayConn interface {
--- internal/conn/conn_test.go
+++ internal/conn/conn_test.go
@@ -14,7 +14,6 @@ func TestInnerConn_ReadWrite(t *testing.T) {
 	testData := []byte("hello")
 
 	clientConn, serverConn := net.Pipe()
-	// 设置一个小的超时时长，以便在没有数据交换时测试可以快速失败而不是永久阻塞
 	clientConn.SetDeadline(time.Now().Add(1 * time.Second))
 	serverConn.SetDeadline(time.Now().Add(1 * time.Second))
 	defer clientConn.Close()
@@ -22,35 +21,30 @@ func TestInnerConn_ReadWrite(t *testing.T) {
 
 	innerC := &innerConn{Conn: clientConn, stats: &Stats{}, remoteLabel: "test"}
 
-	errChan := make(chan error, 1) // 用于从 goroutine 向主流程报告错误
+	errChan := make(chan error, 1)
 
-	// 测试 innerConn.Write
 	go func() {
 		_, err := innerC.Write(testData)
-		errChan <- err // 将错误发送回主流程
+		errChan <- err
 	}()
 
 	buf := make([]byte, len(testData))
 	n, err := serverConn.Read(buf)
 	if err != nil {
-		t.Fatalf("读操作失败: %v", err)
+		t.Fatalf("read error: %v", err)
 	}
 	assert.Equal(t, n, len(testData))
 	assert.Equal(t, testData, buf)
 
-	// 检查写操作是否出错 -- 这里的关键是，在检查前确定写操作已经完成
 	if err := <-errChan; err != nil {
-		t.Fatalf("写操作失败: %v", err)
+		t.Fatalf("write err: %v", err)
 	}
-	// 由于此时已确定写操作完成，可以安全地检查stats
 	assert.Equal(t, int64(len(testData)), innerC.stats.Up)
 
-	// 重置错误通道和超时，准备测试 Read
 	errChan = make(chan error, 1)
 	clientConn.SetDeadline(time.Now().Add(1 * time.Second))
 	serverConn.SetDeadline(time.Now().Add(1 * time.Second))
 
-	// 测试 innerConn.Read
 	go func() {
 		_, err := serverConn.Write(testData)
 		errChan <- err // 将错误发送回主流程
@@ -59,17 +53,16 @@ func TestInnerConn_ReadWrite(t *testing.T) {
 	n, err = innerC.Read(buf)
 	if err != nil {
 		if errors.Is(err, io.EOF) {
-			t.Logf("读取到 EOF，可能是连接关闭")
+			t.Logf("read eof")
 		} else {
-			t.Fatalf("读操作失败: %v", err)
+			t.Fatalf("read error: %v", err)
 		}
 	}
 	assert.Equal(t, n, len(testData))
 	assert.Equal(t, testData, buf)
 
-	// 检查写操作是否出错
 	if err := <-errChan; err != nil {
-		t.Fatalf("写操作失败: %v", err)
+		t.Fatalf("write error: %v", err)
 	}
 	assert.Equal(t, int64(len(testData)), innerC.stats.Down)
 }
