372653374d78994dae3a393d60586b45aa31d0d3
ehco1996 <zh19960202@gmail.com>
2024-05-30 15:03:50 +0800

    metrics: fix for default collector

--- internal/metrics/node.go
+++ internal/metrics/node.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Ehco1996/ehco/internal/config"
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/node_exporter/collector"
@@ -17,6 +18,11 @@ func RegisterNodeExporterMetrics(cfg *config.Config) error {
 	}
 
 	logger := promlog.New(&promlog.Config{Level: level})
+	// node_exporter relay on `kingpin` to enable default node collector
+	// see https://github.com/prometheus/node_exporter/pull/2463
+	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
+		return err
+	}
 	nc, err := collector.NewNodeCollector(logger)
 	if err != nil {
 		return fmt.Errorf("couldn't create collector: %s", err)
